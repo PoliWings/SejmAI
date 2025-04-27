3bc7a08aced182448e0b7e5b351429f5ddbc767e
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2025-04-27 15:15:16 +0200

    Remove unnecessary files

--- alignment-handbook/start.sh
+++ alignment-handbook/start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# nohup ./start.sh &> out.log &
+# ps -ef | grep python
+# pkill -f "python"
+
+# watch -n 0.5 -d free -h
+# watch -n 0.5 -d nvidia-smi
+
 # flash_attn-2.7.3+cu11torch2.3cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
 
 ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/multi_gpu.yaml --num_processes=1 scripts/run_sft.py ../fine_tuning/bielik/left_sft_qlora.yaml
